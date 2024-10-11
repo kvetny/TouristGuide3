34fa3735b52d59e761929a836208246641589dbf
kvetny <sokv0001@stud.kea.dk>
2024-10-11 11:43:34 +0200

    Initial commit

---
+++ src/main/java/kea/sofie/touristguide3/TouristGuide3Application.java
@@ -0,0 +1,13 @@
+package kea.sofie.touristguide3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuide3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristGuide3Application.class, args);
+	}
+
+}
---
+++ src/test/java/kea/sofie/touristguide3/TouristGuide3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.sofie.touristguide3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuide3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
